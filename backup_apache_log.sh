9d44357cf673c4157da3ee37ddfdd5e054f49a8e
Sibin Thomas <54390036+SibinThomasQuad@users.noreply.github.com>
2023-03-22 09:56:46 +0530

    Create backup_apache_log.sh

---
+++ backup_apache_log.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define the log file path
+LOGFILE=/var/log/apache2/access.log
+
+# Define the backup directory
+BACKUP_DIR=/var/log/apache2/backups
+
+# Create the backup directory if it doesn't exist
+if [ ! -d "$BACKUP_DIR" ]; then
+  mkdir -p "$BACKUP_DIR"
+fi
+
+# Define the backup filename with date stamp
+BACKUP_FILE="$BACKUP_DIR/access-$(date +%Y-%m-%d).log"
+
+# Copy the log file to the backup file
+cp "$LOGFILE" "$BACKUP_FILE"
+
+# Clear the log file
+echo "" > "$LOGFILE"
